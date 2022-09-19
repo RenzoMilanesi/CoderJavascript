2a420aed1da21716b523635de7a9379936deb499
Renzo <Renzomilanesi95@hotmail.com>
2022-09-19 19:46:38 -0300

    Subida dinamica preguntas

---
+++ js/carga-questions.js
@@ -0,0 +1,18 @@
+const questionsContainer=document.querySelector('.questions__container')
+
+preguntas.forEach((pregunta)=>{
+    const article=document.createElement('article')
+    article.className="questions__padding";
+    article.innerHTML=`
+    <div class="questions__answer">
+                        <h3 class="questions__title">${pregunta.preguntas_titulo}
+                            <span class="questions__arrow">
+                                <i class="bi bi-arrow-down-circle questions__img" ></i>
+                            </span>
+                        </h3>
+
+                        <p class="questions__show">${pregunta.preguntas_respuetas}</p>
+                    </div>
+    `
+    questionsContainer.append(article)
+})
